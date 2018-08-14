2f5aeb1411f8b796846800c6796717a9b051aaab
Yuhong <yuhongliu15@outlook.com>
2018-08-14 13:49:44 +1000

    Revert "remove the copy file"
    This reverts commit 07fcd5cf89e0d9caf0fb2c2f478ebd948fb6dc51.

---
+++ sample_file copy.py
@@ -0,0 +1,30 @@
+Sample File
+
+
+This is the copy of the initial (first) file
+
+def addition(a, b):
+
+	summation = a + b
+
+	return summation
+
+
+# def subtraction(a, b):
+
+# 	difference = a - b
+
+# 	return difference
+
+
+# def division(a, b):
+
+# 	quotient = a / b
+
+# 	return quotient
+
+# def multiplication(a, b):
+
+# 	product = a * b
+
+# 	return product
